6c77941a0ec1ef28beca831efef32f3f47f2a24b
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-02-23 20:21:28 -0800

    Implemented unit testing
    Added a skeleton for unit testing. And created a test for testing missing files

--- src/test/java/ColumnDisplayTest.java
+++ src/test/java/ColumnDisplayTest.java
@@ -1,2 +1,29 @@
-public class ColumnDisplayTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ColumnDisplayTest {
+    ColumnDisplay columnDisplay;
+
+    @BeforeEach
+    void setUp() {
+        columnDisplay = new ColumnDisplay();
+    }
+
+
+    @Test
+    public void testMissingInputFile() {
+        File file = new File("./input.txt");
+        assertTrue(file.exists(), "File does not exist");
+    }
+
+
+    @AfterEach
+    void tearDown() {
+    }
 }
+
