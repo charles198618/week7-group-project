1fce9880a09f3fd27fdcb03123e5e5f737a2cec7
LJB0695 <barrettny4@gmail.com>
2023-02-24 09:21:50 -0500

    Added a test in the test file to check if the columns set up correctly.

--- src/test/java/ColumnDisplayTest.java
+++ src/test/java/ColumnDisplayTest.java
@@ -3,7 +3,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ColumnDisplayTest {
@@ -12,6 +14,27 @@ class ColumnDisplayTest {
     @BeforeEach
     void setUp() {
         columnDisplay = new ColumnDisplay();
+    }
+
+    @Test
+    void printRow() {
+        // Set up test columns
+        java.util.List<java.util.List<String>> columns = new ArrayList<>();
+        columns.add(new ArrayList<>(java.util.List.of("One", "Two", "Three")));
+        columns.add(new ArrayList<>(java.util.List.of("Four", "Five")));
+        columns.add(new ArrayList<>(java.util.List.of("Six")));
+
+        String rowOneExpected = "One            Four           Six";
+        String rowTwoExpected = "Two            Five";
+        String rowThreeExpected = "Three                                        ";
+
+        // Test each row separately
+        assertEquals(rowOneExpected.trim(),
+                columnDisplay.getRow(columns, 0).trim());
+        assertEquals(rowTwoExpected.trim(),
+                columnDisplay.getRow(columns, 1).trim());
+        assertEquals(rowThreeExpected.trim(),
+                columnDisplay.getRow(columns, 2).trim());
     }
 
 
